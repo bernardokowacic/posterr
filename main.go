f4422b4fd06597797fe481cde5d43f111451ac3c
bernardokowacic <berkowacic@gmail.com>
2022-11-02 11:19:45 -0300

    fix: change dependency location

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"posterr/database"
 	"posterr/repository"
 	"posterr/routes"
+	"posterr/services/post"
+	"posterr/services/user"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -30,7 +32,9 @@ func main() {
 
 	postRepository := repository.NewPostRepository(dbConn)
 	userRepository := repository.NewUserRepository(dbConn)
+	postService := post.NewService(postRepository, userRepository)
+	userService := user.NewService(postRepository, userRepository)
 
-	routes.GetRoutes(router, postRepository, userRepository)
+	routes.GetRoutes(router, postService, userService)
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
