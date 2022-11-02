22ec4bf5fcd22c8041d5f01335c7bb7b4989304e
bernardokowacic <berkowacic@gmail.com>
2022-11-02 14:10:42 -0300

    feat: add test cases to user controller

---
+++ controllers/user_test.go
@@ -0,0 +1,66 @@
+package controllers_test
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"posterr/database/entity"
+	"posterr/routes"
+	"testing"
+	"time"
+
+	postServiceMock "posterr/mocks/services/post"
+	userServiceMock "posterr/mocks/services/user"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserRoute(t *testing.T) {
+	userService := &userServiceMock.UserServiceInteface{}
+	uuid := "53d83955-16f0-445a-bfe6-a431c1789994"
+	foundUser := entity.User{
+		ID:         1,
+		Username:   "Test User",
+		Uuid:       "53d83955-16f0-445a-bfe6-a431c1789994",
+		Created_at: time.Now().Round(0),
+		TotalPosts: 1,
+	}
+
+	router := routes.StartAPI(&postServiceMock.PostServiceInterface{}, userService)
+	userService.On("Find", uuid).Return(foundUser, nil)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/user", nil)
+	req.Header.Set("Authorization", uuid)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	// assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestGetUserRouteWithError(t *testing.T) {
+	userService := &userServiceMock.UserServiceInteface{}
+	uuid := "53d83955-16f0-445a-bfe6-a431c1789994"
+
+	router := routes.StartAPI(&postServiceMock.PostServiceInterface{}, userService)
+	userService.On("Find", uuid).Return(entity.User{}, errors.New("error"))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/user", nil)
+	req.Header.Set("Authorization", uuid)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusInternalServerError, w.Code)
+	// assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestGetUserRouteWithoutAuthorization(t *testing.T) {
+	router := routes.StartAPI(&postServiceMock.PostServiceInterface{}, &userServiceMock.UserServiceInteface{})
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/user", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusForbidden, w.Code)
+	// assert.Equal(t, "pong", w.Body.String())
+}
