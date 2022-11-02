d7bdceab838b9a1727c084bfb822550562097b16
bernardokowacic <berkowacic@gmail.com>
2022-11-02 11:18:25 -0300

    refactor: add error treatment to services

--- services/post/post.go
+++ services/post/post.go
@@ -17,21 +17,36 @@ type postService struct {
 	UserRepository repository.UserRepositoryInterface
 }
 
-func NewPost(postRepository repository.PostRepositoryInterface, userRepository repository.UserRepositoryInterface) PostServiceInterface {
+func NewService(postRepository repository.PostRepositoryInterface, userRepository repository.UserRepositoryInterface) PostServiceInterface {
 	return &postService{PostRepository: postRepository, UserRepository: userRepository}
 }
 
 func (p *postService) Index(userUuid string, owner string, page uint64, pageSize uint64, startDateFormated *time.Time, endDateFormated *time.Time) ([]entity.Post, error) {
-	user := p.UserRepository.GetUserData(userUuid)
+	if page == 0 {
+		return nil, errors.New("page can not be 0")
+	}
+	user, err := p.UserRepository.GetUserData(userUuid)
+	if err != nil {
+		return nil, err
+	}
 
-	posts := p.PostRepository.Get(user.ID, owner, page, pageSize, startDateFormated, endDateFormated)
+	posts, err := p.PostRepository.Get(user.ID, owner, page, pageSize, startDateFormated, endDateFormated)
+	if err != nil {
+		return nil, err
+	}
 	return posts, nil
 }
 
 func (p *postService) Insert(userUuid string, content string, repostID uint64, quotePostID uint64) error {
-	user := p.UserRepository.GetUserData(userUuid)
+	user, err := p.UserRepository.GetUserData(userUuid)
+	if err != nil {
+		return err
+	}
 
-	currentDayPostsCount := p.PostRepository.CurrentDayTotalPosts(user.ID)
+	currentDayPostsCount, err := p.PostRepository.CurrentDayTotalPosts(user.ID)
+	if err != nil {
+		return err
+	}
 	if currentDayPostsCount > 5 {
 		err := errors.New("maximum number of posts reached")
 		return err
@@ -40,7 +55,11 @@ func (p *postService) Insert(userUuid string, content string, repostID uint64, q
 	var repost *uint64
 	var quotePost *uint64
 	if repostID != 0 {
-		if !p.PostRepository.CheckUniqueRepost(repostID) {
+		uniqueRepost, err := p.PostRepository.CheckUniqueRepost(repostID)
+		if err != nil {
+			return err
+		}
+		if !uniqueRepost {
 			err := errors.New("the current reposted post reached the maximum number of reposts")
 			return err
 		}
@@ -48,7 +67,11 @@ func (p *postService) Insert(userUuid string, content string, repostID uint64, q
 		repost = &repostID
 	}
 	if quotePostID != 0 {
-		if !p.PostRepository.CheckUniqueQuotes(repostID) {
+		uniqueQuote, err := p.PostRepository.CheckUniqueQuotes(repostID)
+		if err != nil {
+			return err
+		}
+		if !uniqueQuote {
 			err := errors.New("the current quoted post reached the maximum number of quotes")
 			return err
 		}
--- services/user/user.go
+++ services/user/user.go
@@ -6,7 +6,7 @@ import (
 )
 
 type UserServiceInteface interface {
-	Find(userUuid string) entity.User
+	Find(userUuid string) (entity.User, error)
 }
 
 type UserService struct {
@@ -14,13 +14,20 @@ type UserService struct {
 	UserRepository repository.UserRepositoryInterface
 }
 
-func NewUser(postRepository repository.PostRepositoryInterface, userRepository repository.UserRepositoryInterface) UserServiceInteface {
+func NewService(postRepository repository.PostRepositoryInterface, userRepository repository.UserRepositoryInterface) UserServiceInteface {
 	return &UserService{PostRepository: postRepository, UserRepository: userRepository}
 }
 
-func (u *UserService) Find(userUuid string) entity.User {
-	user := u.UserRepository.GetUserData(userUuid)
-	user.TotalPosts = u.PostRepository.CountUserTotalPosts(user.ID)
+func (u *UserService) Find(userUuid string) (entity.User, error) {
+	var err error
+	user, err := u.UserRepository.GetUserData(userUuid)
+	if err != nil {
+		return entity.User{}, err
+	}
+	user.TotalPosts, err = u.PostRepository.CountUserTotalPosts(user.ID)
+	if err != nil {
+		return user, err
+	}
 
-	return user
+	return user, nil
 }
